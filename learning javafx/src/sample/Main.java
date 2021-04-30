c7c66ee4259c71861e8998c3cb1f3973ca02d020
3levate <58492326+3levate@users.noreply.github.com>
2021-04-30 14:30:13 -0700

    Add files via upload

--- learning javafx/src/sample/Main.java
+++ learning javafx/src/sample/Main.java
@@ -1,23 +1,69 @@
 package sample;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) {
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25,25,25,25));
+
+        Scene scene = new Scene(grid, 300, 275);
+        Text scenetitle = new Text("Login");
+        scenetitle.setFont(Font.font("Calibri", FontWeight.NORMAL, 20));
+        Label username = new Label("Username");
+        TextField userTextField = new TextField();
+        Label password = new Label("Password");
+        PasswordField passwordBox = new PasswordField();
+
+        grid.add(scenetitle, 0, 0, 2, 1);
+        grid.add(username, 0, 1);
+        grid.add(userTextField, 1, 1);
+        grid.add(password, 0, 2);
+        grid.add(passwordBox, 1, 2);
+
+        grid.setGridLinesVisible(false);
+
+        Button button = new Button("Sign in");
+        HBox hbButton = new HBox(10);
+        hbButton.setAlignment(Pos.BOTTOM_RIGHT);
+        hbButton.getChildren().add(button);
+        grid.add(hbButton, 1, 4);
+
+        final Text actionTarget = new Text();
+        grid.add(actionTarget, 1, 6);
+
+        button.setOnAction(event ->
+        {
+            actionTarget.setFill(Color.FIREBRICK);
+            actionTarget.setText("Sign in pressed");
+        });
+        scene.getStylesheets().add(Main.class.getResource("Login.css").toExternalForm());
+        primaryStage.setTitle("JavaFX login form");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
+        //goo.gl/1rkzka
     }
 }
