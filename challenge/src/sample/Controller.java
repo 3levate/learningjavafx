894ebb2c765fde1572c029d0cb524b51401227dd
3levate <58492326+3levate@users.noreply.github.com>
2021-05-03 08:46:26 -0700

    Add files via upload

--- challenge/src/sample/Controller.java
+++ challenge/src/sample/Controller.java
@@ -1,6 +1,7 @@
 package sample;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,11 +15,42 @@ public class Controller implements Initializable {
     private Label label;
     @FXML
     private Text actionTarget;
+    @FXML
+    int whatText2;
+    int whatText1 = 0;
 
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        actionTarget.setText("asopjdfa;oifgo;adghiopardsjgkl;aejgko;lsdjg");
+        Random random = new Random();
+        whatText2 = random.nextInt(4 - 1 + 1) + 1;
+        if (Integer.compare(whatText1, whatText2) != 0) {
+            whatText1 = whatText2;
+        }
+        else {
+            whatText2 = random.nextInt(4 - 1 + 1) + 1;
+        }
+        System.out.println(whatText1);
+        System.out.println(whatText2);
+        switch (whatText1) {
+            case 1:
+                actionTarget.setText("What is your name?");
+                System.out.println("case 1");
+                break;
+            case 2:
+                actionTarget.setText("How are you doing?");
+                System.out.println("case 2");
+                break;
+            case 3:
+                actionTarget.setText("Favourite food?");
+                System.out.println("case 3");
+                break;
+            case 4:
+                actionTarget.setText("What are you having for dinner today?");
+                System.out.println("case 4");
+                break;
+        }
+        whatText1 = whatText1;
     }
 
     @Override
